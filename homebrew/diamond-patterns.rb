ee4a5fb197b386ca6d034fdbc7e3a7d3f8dd0382
Ian Dennis Miller <ian@iandennismiller.com>
2016-09-22 13:03:32 -0400

    fiddle with homebrew

--- homebrew/diamond-patterns.rb
+++ homebrew/diamond-patterns.rb
@@ -2,6 +2,8 @@ class DiamondPatterns < Formula
   desc "Diamond Patterns for creating projects"
   homepage "https://github.com/iandennismiller/diamond-patterns"
   # url "https://pypi.python.org/packages/d9/47/9b2449abffbfbdd77104cdd2ce11b2ecf5cbc37363bd3bbe55ed4f352094/Diamond-Patterns-0.1.1.tar.gz"
+  version "0.2.0"
+  # version_scheme 1
   url "https://github.com/iandennismiller/diamond-patterns/archive/master.zip"
   # sha256 "9b2449abffbfbdd77104cdd2ce11b2ecf5cbc37363bd3bbe55ed4f352094"
 
