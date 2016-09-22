24a3dd7654343027bfd06c68c7393a872d608319
Ian Dennis Miller <ian@iandennismiller.com>
2016-09-22 14:26:17 -0400

    install differently

--- homebrew/diamond-patterns.rb
+++ homebrew/diamond-patterns.rb
@@ -1,22 +1,21 @@
 class DiamondPatterns < Formula
   desc "Diamond Patterns for creating projects"
   homepage "https://github.com/iandennismiller/diamond-patterns"
-  version_scheme 1
   url "https://github.com/iandennismiller/diamond-patterns/archive/0.2.0.tar.gz"
   sha256 "5830388ac6b2b6b74fa3818ecc68e3a765418f4e22126cd17abdb709b0041ba9"
 
-  head "https://github.com/iandennismiller/diamond-patterns.git"
-
   bottle :unneeded
 
-  depends_on "pyenv"
-
   def install
-    ENV["PREFIX"] = prefix
-    system "make install"
-  end
-
-  test do
-    shell_output("diamond -h")
+    bin.install "bin/brew-pip"
+    bin.install "bin/diamond"
+    bin.install "bin/diamond-conf-init"
+    bin.install "bin/diamond-upgrade"
+    bin.install "bin/project-git-init.sh"
+    bin.install "bin/project-list"
+    bin.install "bin/project-new"
+    bin.install "bin/project-new3"
+    bin.install "bin/project-open.sh"
+    bin.install "bin/project-workon"
   end
 end
