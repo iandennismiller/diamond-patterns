9600945d00349f49bc55be4284ba3e236e154bdd
Ian Dennis Miller <ian@iandennismiller.com>
2017-03-12 22:25:52 -0400

    change where the newlines are

--- diamond_patterns/skels/blog/_bin/git/init.sh
+++ diamond_patterns/skels/blog/_bin/git/init.sh
@@ -4,30 +4,30 @@
 source .settings.conf
 
 echo "create bare git repository on remote host"
-echo
 ssh ${SSH_USER}@${SSH_HOST} "git init --bare ${GIT_REPO}"
 echo "OK"
+echo
 
 echo "initialize local blog folder and push to repository on remote host"
-echo
 git init
 git remote add origin ${SSH_USER}@${SSH_HOST}:${GIT_REPO}
 git add -A
 git commit -am "initialize repository with fresh scaffold"
 git push -u origin master
 echo "OK"
+echo
 
 echo "create working copy of blog on remote host"
-echo
 ssh ${SSH_USER}@${SSH_HOST} "git clone ${GIT_REPO} ${WORKING_PATH}"
 echo "OK"
+echo
 
 echo "install git hooks on remote host"
-echo
 ssh ${SSH_USER}@${SSH_HOST} "cd ${WORKING_PATH} && make githooks"
 echo "OK"
+echo
 
 echo "build jekyll dependencies on remote host"
-echo
 ssh ${SSH_USER}@${SSH_HOST} "cd ${WORKING_PATH} && make depends"
 echo "OK"
+echo
