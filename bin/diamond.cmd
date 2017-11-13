4935a8f1f522119942e5fa0e829a66f4531ca1d1
Ian Dennis Miller <ian@iandennismiller.com>
2017-11-13 09:48:43 -0500

    working windows invocation

--- bin/diamond.cmd
+++ bin/diamond.cmd
@@ -4,8 +4,8 @@ REM This is a Windows batch file for launching diamond-patterns
 REM The script assumes you are using a python virtualenv.
 
 IF NOT DEFINED VIRTUAL_ENV (
-    for /f "delims=" %%A in ('where diamond') do set "DIAMOND_PATH=%%A"
-    python.exe %DIAMOND_PATH% %*
+    for /f "delims=" %%A in ('where diamond.cmd') do set "DIAMOND_PATH=%%A"
+    python.exe "%DIAMOND_PATH:~,-4%" %*
 ) ELSE (
     %VIRTUAL_ENV%\scripts\python.exe %VIRTUAL_ENV%\scripts\diamond %*
 )
