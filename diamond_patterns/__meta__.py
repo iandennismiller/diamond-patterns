0248a3ffb2177bc018e6a2f08cb5e7b539b0194c
Ian Dennis Miller <ian@iandennismiller.com>
2018-04-24 09:23:04 -0400

    article products

--- diamond_patterns/__meta__.py
+++ diamond_patterns/__meta__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3.5rc1'
+__version__ = '0.3.5rc2'
 __project__ = 'diamond-patterns'
 __author__ = 'Ian Dennis Miller'
 __email__ = 'iandennismiller@gmail.com'
